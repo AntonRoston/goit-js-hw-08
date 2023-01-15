56f37909b354ae77d704b1f49caab5c6da3cce9d
AntonRoston <rostonanton@gmail.com>
2023-01-15 15:25:03 +0200

    01_02_

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const divConteiner = document.querySelector('.gallery');
+const cardsMarkup = createGalleryMarkup(galleryItems);
+
+divConteiner.insertAdjacentHTML('beforeend', cardsMarkup);
+
+divConteiner.addEventListener('click', onConteinerClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` 
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+      `;
+    })
+    .join('');
+}
+
+function onConteinerClick(evt) {
+  evt.preventDefault();
+  const isImg = evt.target.classList.contains('gallery__image');
+  console.log(evt.target);
+  if (isImg) {
+    //   console.log(evt.target.a);
+  }
+
+  // console.log(evt.target);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    localStorage.setItem('key_seconds', currentTime.seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('key_seconds') || 0;
+
+player.setCurrentTime(currentTime);
